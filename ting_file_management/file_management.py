f26745ddb1d2f296db54b6a01779117e1135f9dd
Michel Pereira <michelbecauro@gmail.com>
2022-04-12 08:53:35 -0300

    feat: REQ. 2 - Implements stderr for file reading in txt_importer()

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,13 +1,22 @@
+import sys
+
+
 def txt_importer(path_file):
     file_lines = []
     file_lines_without_n = []
 
-    with open(path_file, mode="r") as file:
-        for line in file:
-            file_lines.append(line)
+    if ".txt" not in path_file:
+        return sys.stderr.write("Formato inválido\n")
+    else:
+        try:
+            with open(path_file, mode="r") as file:
+                for line in file:
+                    file_lines.append(line)
 
-    file_lines_without_n = "".join(file_lines).split("\n")
-    return file_lines_without_n
+            file_lines_without_n = "".join(file_lines).split("\n")
+            return file_lines_without_n
+        except Exception:
+            sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
 
 
 if __name__ == "__main__":
