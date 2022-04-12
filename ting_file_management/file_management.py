ad40e0c725e36429e7db1dcbf8e7fceb6246d4f4
Michel Pereira <michelbecauro@gmail.com>
2022-04-12 08:02:20 -0300

    feat: REQ. 2 - Implements txt file reader in txt_importer()

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    file_lines = []
+
+    with open(path_file, mode="r") as file:
+        for line in file:
+            file_lines.append(line)
+
+    return file_lines
+
+
+if __name__ == "__main__":
+
+    print(txt_importer("statics/arquivo_teste.txt"))
